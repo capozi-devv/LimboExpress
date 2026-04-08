f2a7f84e3fd7af7229022b171e9c7477c45f53a0
Capozi <capozi.devv@gmail.com>
2026-04-07 22:25:54 -0400

    starting background music

---
+++ src/main/java/net/capozi/limbo_express/mixin/WatheClientMixin.java
@@ -0,0 +1,24 @@
+package net.capozi.limbo_express.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.doctor4t.wathe.cca.GameWorldComponent;
+import dev.doctor4t.wathe.client.WatheClient;
+import net.capozi.limbo_express.common.cca.PlayerAnonymityComponent;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(WatheClient.class)
+public class WatheClientMixin {
+    @WrapOperation(method = "getInstinctHighlight", at = @At(value = "INVOKE", target = "Ldev/doctor4t/wathe/cca/GameWorldComponent;isInnocent(Lnet/minecraft/entity/player/PlayerEntity;)Z"))
+    private static boolean limboExpress$getInstinctHighlightCivilian(GameWorldComponent instance, PlayerEntity player, Operation<Boolean> original) {
+        PlayerAnonymityComponent component = PlayerAnonymityComponent.KEY.get(player);
+        return !component.isAnonymous() && original.call(instance, player);
+    }
+    @WrapOperation(method = "getInstinctHighlight", at = @At(value = "INVOKE", target = "Ldev/doctor4t/wathe/cca/GameWorldComponent;canUseKillerFeatures(Lnet/minecraft/entity/player/PlayerEntity;)Z"))
+    private static boolean limboExpress$getInstinctHighlightKiller(GameWorldComponent instance, PlayerEntity player, Operation<Boolean> original) {
+        PlayerAnonymityComponent component = PlayerAnonymityComponent.KEY.get(player);
+        return !component.isAnonymous() && original.call(instance, player);
+    }
+}
