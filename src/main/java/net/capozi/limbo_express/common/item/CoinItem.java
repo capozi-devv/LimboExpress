5468c52c2bf40e64e6206bac3b2d4aae6b6577f3
Capozi <capozi.devv@gmail.com>
2026-07-27 10:34:45 -0400

    Update CoinItem.java

--- src/main/java/net/capozi/limbo_express/common/item/CoinItem.java
+++ src/main/java/net/capozi/limbo_express/common/item/CoinItem.java
@@ -1,27 +1,18 @@
 package net.capozi.limbo_express.common.item;
 
 import dev.doctor4t.wathe.cca.PlayerShopComponent;
-import dev.doctor4t.wathe.index.WatheSounds;
 import net.capozi.limbo_express.foundation.DataComponentTypeInit;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.StackReference;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.tooltip.TooltipType;
-import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
-import net.minecraft.registry.Registries;
 import net.minecraft.screen.slot.Slot;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.sound.SoundCategory;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.ClickType;
-import net.minecraft.util.Formatting;
-import net.minecraft.world.World;
 
 import java.util.List;
-import java.util.Random;
 
 public class CoinItem extends Item {
     public CoinItem(Settings settings) {
